8561b421496593ee3c91dd8038faba21436902d4
kirillsankov <73188231+kirillsankov@users.noreply.github.com>
2022-11-28 17:34:12 +0300

    Add files via upload

--- New folder (2)/script.js
+++ New folder (2)/script.js
@@ -133,17 +133,11 @@ const SETTINGS = {
             ]
         }
     ],
+    EMAIL_REGEXP: /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
+    PHONE_REGEXP: /^[\d\+][\d\(\)\ -]{4,14}\d$/,
 }
 class Page {
     constructor() {
-        this.logo =  document.querySelector(".nav__logo");
-        this.header = document.querySelector(".header-bottom");
-        this.sections = document.querySelectorAll('section');
-        this.button = document.querySelector(".realty__button");
-        this.isSticky = false;
-        this.addVideo();
-        this.clickTab();
-        this.openMenuBurger();
         this.validateForm();
     }
     validateForm() {
@@ -157,8 +151,11 @@ class Page {
            if(this.isRequired([inputName, inputPhone, inputEmail, inputSelect])
                && this.inputMaxLength(inputName, 30)
                && this.inputMinLength(inputName, 2)
-               &&) {
-
+               && this.isValidEmail(inputEmail)
+               && this.isValidPhone(inputPhone)) {
+                alert("Все правильно");
+           } else {
+               alert("Ошибка");
            }
         });
     }
@@ -176,94 +173,12 @@ class Page {
     inputMinLength(input, min) {
         return input.value.length > min;
     }
-    isValidEmail() {
+    isValidEmail(input) {
+        return SETTINGS.EMAIL_REGEXP.test(input.value);
+    }
+    isValidPhone(input) {
+        return SETTINGS.PHONE_REGEXP.test(input.value);
+    }
 
-    }
-    openMenuBurger() {
-        let menu = document.querySelector(".nav__menuburger");
-        let nav = document.querySelector(".nav");
-
-        menu.addEventListener('click', () => {
-            nav.classList.toggle('nav__open');
-            menu.classList.toggle('nav__menu-open');
-            if(menu.classList.contains("nav__menu-open") && !this.isSticky)this.header.style.transform = `translateY(${this.header.getBoundingClientRect().top}px)`;
-            else if(menu.classList.contains("nav__menu-open") && this.isSticky) this.logo.classList.toggle('anim');
-            else this.header.style.transform = "translateY(0px)";
-            document.body.classList.toggle('lock');
-        })
-    }
-    initMap() {
-        const map = new google.maps.Map(document.querySelector(".map"), {
-            zoom: 15,
-            center: {
-                lat: 41.896412525779375,
-                lng: -87.63790018837435
-            },
-            disableDefaultUI: true,
-            styles: SETTINGS.mapStyle,
-        });
-        const marker = new google.maps.Marker({
-            position:  {
-                lat: 41.896412525779375,
-                lng: -87.63790018837435
-            },
-            map: map,
-            icon: "https://uploads-ssl.webflow.com/637b452d03cd693dd90b2bbc/637f5797bac0ffacb289b66a_pin.png",
-        });
-    }
-    resizeLogo() {
-        if(this.header.getBoundingClientRect().top === 0) {
-            this.logo.classList.add('anim');
-            this.isSticky = true;
-        }
-        else {
-            this.logo.classList.remove('anim');
-            this.isSticky = false;
-        }
-    }
-    changeColor() {
-        let headerY = this.header.getBoundingClientRect().bottom + window.scrollY;
-        for(let section of this.sections) {
-            let sectionYTop = section.getBoundingClientRect().top + window.scrollY;
-            let sectionYBottom = section.getBoundingClientRect().bottom + window.scrollY;
-            if (sectionYTop < headerY && sectionYBottom > headerY) {
-                (section.dataset.color === "white") ? this.header.classList.add("white") : this.header.classList.remove("white");
-            }
-        }
-    }
-    addVideo() {
-        let video = "<iframe width=\"594\" height=\"416\" src=\"https://www.youtube.com/embed/iUVNspaiBAo\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
-        let realty = document.querySelector(".realty__right");
-        this.button.addEventListener('click', () => {
-            realty.style.height = realty.clientHeight + "px";
-            document.querySelector(".realty__img").remove();
-            this.button.remove();
-            realty.innerHTML = video;
-        })
-    }
-    clickTab() {
-        let cross = document.querySelectorAll(".cross");
-        cross.forEach((el)=> {
-            el.addEventListener("click", () => {
-                el.parentNode.querySelector('.tab__hidden').classList.toggle("tabs__opened");
-                el.querySelector(".cross__item__rotate90").classList.toggle("cross__item__opacity");
-            });
-        });
-    }
 }
 let page = new Page();
-addEventListener("scroll", () => {
-    page.resizeLogo();
-    page.changeColor();
-});
-window.initMap = page.initMap;
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    autoplay: {
-        delay: 5000,
-    },
-    navigation: {
-        nextEl: '.arrow__right',
-        prevEl: '.arrow__left',
-    },
-});
